8717da290758a0ab78d2a13de0e3d95252f20439
Mostafijur Rahman <mostafijur1812@gmail.com>
2025-05-10 13:28:26 +0600

    models, entiry framework added

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab11f21c92675580ab24e5f0714bdab4e9b3eef9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8a15bc413e3722fc2331ad4aa12e86d0ec6057b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
