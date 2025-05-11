599cc9c5cbd68c27c262bba4399fd01f4a78ad57
Mostafijur Rahman <mostafijur1812@gmail.com>
2025-05-12 00:38:36 +0600

    put request added

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -49,6 +49,28 @@ namespace api.Controllers
             return CreatedAtAction(nameof(GetById), new {id = stockModel.Id}, stockModel.toStockDto());
         }
 
+        [HttpPut("{id}")]
+        public IActionResult Update([FromRoute] int id,[FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stockModel = _context.Stock.FirstOrDefault(x => x.Id == id);
+
+            if(stockModel == null) 
+            {
+                return NotFound();
+            }
+
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.MarketCap = updateDto.MarketCap;
+
+            _context.SaveChanges();
+
+            return Ok(stockModel.toStockDto());
+        }
+
          [HttpDelete("{id}")]
         public IActionResult Delete([FromRoute] int id)
         {
--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee13b66e2666b2ef5d3fb390b863e29d4aab8009")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+092cc74d1cc15df73762a64600520a1414c68607")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
