a5f78f8fbad075c86bccaef45b3273c495002427
Mostafijur Rahman <mostafijur1812@gmail.com>
2025-05-17 20:47:37 +0600

    comment system

---
+++ api/Repository/CommentRepsitory.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Interfaces;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repository
+{
+    public class CommentRepsitory : ICommentRepository
+    {
+        private readonly ApplicationDBContext _context;
+        public CommentRepsitory(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Comment>> GetAllAsync()
+        {
+            return await _context.Comments.ToListAsync();
+        }
+    }
+}
--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cda2b21d0e68f41e41e107fd58d7acfe551f1d66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47bc8ccc18d088e15dbdaae9d9f6764fff339259")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
